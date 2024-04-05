1caa30a8486c438143c19676deecfcb27b41577b
SebastianNeo <sebastiangear2018@gmail.com>
2024-04-04 22:22:34 -0500

    Se agregar archivo conversion.py

---
+++ TallerPython/Conversiones_tipos_Datos.py
@@ -0,0 +1,22 @@
+#Conversiones de Tipos de Datos
+
+#dato = int(input("Ingrese Edad: "))
+#print(dato)
+#print(type(dato))
+#resultado = dato + 1
+#print(resultado)
+
+#dato = float(input("Ingrese la altura: "))
+#print(dato)
+#print(type(dato))
+#resultado = dato + 1
+#print(resultado)
+
+
+#dato = 55
+#dato2 = str(dato)
+#print(type(dato2))
+
+dato = "COLOMBIA"
+lista = list(dato)
+print(lista)
