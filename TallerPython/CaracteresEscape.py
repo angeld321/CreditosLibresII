bc889956e1c33e4fe49490dc6e238271ffa22901
SebastianNeo <sebastiangear2018@gmail.com>
2024-04-04 22:35:10 -0500

    Se agrego ejemplos de caracteres de escape.py

---
+++ TallerPython/CaracteresEscape.py
@@ -0,0 +1,14 @@
+#Caracteres de escape
+
+#print("Hola\nmundo") #Representar un salto de línea.
+
+#print("Hola\tmundo") #Representar un tabulador:
+
+#print("Este es un backslash: \\") #Representa una barra invertida
+
+#Representa comillas dobles o simples dentro de una cadena.
+print("Este es un ejemplo de comillas: \"Texto entre comillas\"") 
+print('Otra forma de usar comillas dentro de una cadena: \'Texto entre comillas\'')
+
+
+
